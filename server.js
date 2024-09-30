7679b194299928961a862f8e8648c517ab42bcca
Rohan Gadakh <109667271+rohangadakh@users.noreply.github.com>
2024-09-30 22:42:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const { chromium } = require("playwright");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://tubegains-render.onrender.com'], 
+  methods: ['GET', 'POST'], 
+  credentials: true, 
+}));
+
 app.use(express.json());
 
 app.post("/check-yt-ad", async (req, res) => {
